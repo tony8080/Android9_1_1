d9ff3873a73591fee9b5b39d95b919f69c845de2
Kentaro Ooka <kooka@eink.com>
2016-08-02 17:53:33 +0900

    ダイアログ表示

---
+++ app/src/main/java/com/example/android9_1_1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.android9_1_1;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = ( Button )findViewById( R.id.button );
+        button.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View view )
+            {
+                AlertDialog.Builder dialog = new AlertDialog.Builder( MainActivity.this );
+
+                dialog.setTitle("Alart Dialog OK");
+                dialog.setMessage("OKボタンをクリック");
+//              dialog.setIcon( R.drawable.ic_launcher );
+                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick( DialogInterface dialog, int which )
+                    {
+                        TextView textView = ( TextView )findViewById( R.id.textView );
+                        textView.setText("OK Click");
+                    }
+                });
+                dialog.show();
+            }
+        });
+    }
+}
